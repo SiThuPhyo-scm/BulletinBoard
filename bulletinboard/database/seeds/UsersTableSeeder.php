eae4c6246c2b6d977f9ec08ee930826a1642ecc9
scmstp <scm.sithuphyo@gmail.com>
2019-12-30 14:58:32 +0630

    createproject

---
+++ bulletinboard/database/seeds/UsersTableSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            [
+                'name' => 'admin',
+                'email' => 'admin@gmail.com',
+                'password' => bcrypt('123456'),
+                'created_at' => date('Y-m-d H:i:s'),
+            ],
+        ]);
+    }
+}
