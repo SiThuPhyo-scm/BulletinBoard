84608049ba7e986f1159b29cf07c7ffd90e9c466
scmstp <scm.sithuphyo@gmail.com>
2020-01-03 15:17:27 +0630

    1.3.20_Migration&SeedCreate

--- bulletinboard/resources/views/layouts/app.blade.php
+++ bulletinboard/resources/views/layouts/app.blade.php
@@ -23,43 +23,52 @@
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    SCM BulletinBoard
-                </a>
+                <h1>
+                    SCM Bulletin Board
+                </h1>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
                 </button>
 
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
-
-                    </ul>
+                    @guest
+                        @else
+                        <ul class="navbar-nav mr-auto">
+                            <li class="nav-item">
+                                <a class="nav-link" href="userlist">Users</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="user">User</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="postlist">Posts</a>
+                            </li>
+                        </ul>
 
                     <!-- Right Side Of Navbar -->
                     <ul class="navbar-nav ml-auto">
                         <!-- Authentication Links -->
-                        @guest
-                        @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }} <span class="caret"></span>
+
+                            <li class="nav-item">
+                                <label class="nav-link" for="AuthUser">
+                                    {{ Auth::user()->name }}
+                                </label>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('logout') }}"
+                                    onclick="event.preventDefault();
+                                    document.getElementById('logout-form').submit();">
+                                    {{ __('Logout') }}
                                 </a>
 
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                        @csrf
-                                    </form>
-                                </div>
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                    @csrf
+                                </form>
                             </li>
-                        @endguest
+
                     </ul>
+                    @endguest
                 </div>
             </div>
         </nav>
@@ -67,6 +76,21 @@
         <main class="py-4">
             @yield('content')
         </main>
+        @guest
+        @else
+        <footer class="page-footer">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-10">
+                        <p>Seattle Consulting Myanmar</p>
+                    </div>
+                    <div class="col-md-2">
+                        <p>&copy copyright</p>
+                    </div>
+                </div>
+            </div>
+        </footer>
+        @endguest
     </div>
 </body>
 </html>
